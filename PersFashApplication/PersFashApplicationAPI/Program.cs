1c0d280001d5aec02ebd16b68b63043cda90bbf6
Lê Văn Nguyên <levannguyen2003@gmail.com>
2024-10-04 19:38:06 +0700

    [Update] Add Swagger UI

--- PersFashApplication/PersFashApplicationAPI/Program.cs
+++ PersFashApplication/PersFashApplicationAPI/Program.cs
@@ -240,11 +240,14 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
